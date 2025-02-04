0deac025e00d85b80ce2cfed6ced2b0ed2ef03d2
davidyap <davidyapsy@gmail.com>
2025-02-04 16:34:21 +0800

    2025 bd _v1

--- aien_bd/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("specialButton").addEventListener("click", function() {
-    const surpriseMessage = document.getElementById("surpriseMessage");
-    surpriseMessage.style.display = "block";
-});
-  
